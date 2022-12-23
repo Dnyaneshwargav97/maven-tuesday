266d793d298528cc4c2d27c8bdee44a527a91cac
Dnyaneshwargav97 <dnyaneshwargavade@gmail.com>
2022-12-23 12:54:47 +0530

    this is 2nd commit fo demo

---
+++ src/test/java/maven_thusday/demo.java
@@ -0,0 +1,16 @@
+package maven_thusday;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		System.out.println("hi this is 2nd push code");
+	}
+
+}
